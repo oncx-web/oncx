e049b8930cb56f0e82ea5068c309e93d8b405d54
lorddev <onc.stagram@gmail.com>
2024-08-03 14:52:20 +0700

    sec-6 Add Product category

--- vir_oncx/src/product/models.py
+++ vir_oncx/src/product/models.py
@@ -11,11 +11,24 @@ class Product(models.Model):
     )
 
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=5)
     created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return  self.name
+        return self.name
+    
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
