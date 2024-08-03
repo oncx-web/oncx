fac2fc334d7a35ffba17141a9da42ac14b20a840
lorddev <onc.stagram@gmail.com>
2024-08-03 13:33:28 +0700

    sec-5-Creating Our Products App Model Part 2

--- vir_oncx/src/product/models.py
+++ vir_oncx/src/product/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,10 +11,11 @@ class Product(models.Model):
     )
 
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
     price = models.DecimalField(max_digits=10, decimal_places=5)
-    created = models.DateTimeField()
+    created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return  self.name
