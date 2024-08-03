7a048f36494211de7ddf2ec41f505c327b357216
lorddev <onc.stagram@gmail.com>
2024-08-03 18:53:32 +0700

    sec-7 7. Adding Brand & Product Images Models

--- vir_oncx/src/product/models.py
+++ vir_oncx/src/product/models.py
@@ -15,14 +15,28 @@ class Product(models.Model):
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=5)
     created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
+
+class ProductImages(models.Model):
+
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='products/',blank=True,null=True)
+
+    def __str__(self):
+        return self.product.name
+    class Meta:
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'
+
     
 
 class Category(models.Model):
+    # for category name
     category_name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
 
@@ -32,3 +46,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+
+class Brand(models.Model):
+    # for brand name
+    brand_name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'brand'
+        verbose_name_plural = 'brands'
+
+    def __str__(self):
+        return self.brand_name
